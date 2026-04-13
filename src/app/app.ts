32a040050c24afbd200f5718f362d56923d37fce
bilal <125172509+Bilal09505@users.noreply.github.com>
2026-04-13 12:26:08 +0500

    first changes updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet,Navbar,Footer],
+  template: `
+    <app-navbar></app-navbar>
+    
+    <main class="min-h-screen">
+      <router-outlet></router-outlet>
+    </main>
+    
+    <app-footer></app-footer>
+  `,
 })
 export class App {
-  protected readonly title = signal('chazaz-ltd');
+  
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      primary: "#bc870a",
+      dark: "#0F172A",
+      accent: "#FACC15",
+    }
+    },
+  },
+  plugins: [],
+}
+
