131fe7eb356de61c53796a30b336ccdbb00c3227
arslan <Arsal024@gmail.com>
2026-06-11 15:38:06 +0500

    update for google

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 import { Footer } from './shared/components/footer/footer';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer],
+  imports: [RouterOutlet, Navbar, Footer],
   template: `
     <app-navbar></app-navbar>
     
@@ -17,5 +18,17 @@ import { Footer } from './shared/components/footer/footer';
   `,
 })
 export class App {
-  
+  constructor(private meta: Meta, private title: Title) { }
+
+  ngOnInit() {
+    this.title.setTitle('Chazaz Ltd - Professional Security Services in the UK');
+
+    this.meta.addTags([
+      { name: 'description', content: 'Chazaz Ltd provides professional security guards and security services across the UK. Reliable, trained personnel for businesses, events, and properties.' },
+      { name: 'keywords', content: 'security company UK, security guards UK, professional security services, event security, property security, Chazaz Ltd' },
+      { property: 'og:title', content: 'Chazaz Ltd - Professional Security Services in the UK' },
+      { property: 'og:description', content: 'Chazaz Ltd provides professional security guards and security services across the UK. Reliable, trained personnel for businesses, events, and properties.' },
+      { property: 'og:url', content: 'https://chazaz-ltd-store.web.app/' }
+    ]);
+  }
 }
